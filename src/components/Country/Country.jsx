60545c40c8467570cf36173e470b4b67e9a2e709
fojletuhin <fojletuhin@gmail.com>
2024-03-14 00:38:47 +0600

    add population area

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -5,11 +5,13 @@ const Country = ({country}) => {
 
     console.log(country)
 
-    const {name, flags}=country;
+    const {name, flags, population, area}=country;
     return (
         <div className='Country'>
             <h3>Name:{name.common} </h3>
             <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
         </div>
     );
 };
