826a149cedd32d1dd86566fc339b7c178e127073
fojletuhin <fojletuhin@gmail.com>
2024-03-14 01:22:19 +0600

    going or visit

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './Country.css'
 
 
@@ -5,13 +6,23 @@ const Country = ({country}) => {
 
     console.log(country)
 
-    const {name, flags, population, area}=country;
+    const {name, flags, population, area, cca3}=country;
+
+    const[visited, setVisited]=useState(false);
+    const handleVisited=()=>{
+        setVisited(!visited);
+    }
     return (
         <div className='Country'>
             <h3>Name:{name.common} </h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
+            <p>CCa3: {cca3}</p>
+            <button onClick={handleVisited}>{visited?'visited':'Going'}</button>
+            {
+                visited ? '  I have visited this country':' I want to visit'
+            }
         </div>
     );
 };
